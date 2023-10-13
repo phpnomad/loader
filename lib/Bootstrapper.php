bfe9826e16ba6dfceba80689c5a9593ff1643dae
Alex Standiford <alex@standiford.us>
2023-10-13 08:56:38 -0400

    Corrects missing import

--- lib/Bootstrapper.php
+++ lib/Bootstrapper.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace Phoenix\Loader\Traits;
+namespace Phoenix\Loader;
 
 use Phoenix\Loader\Interfaces\HasClassDefinitions;
 use Phoenix\Facade\Interfaces\HasFacades;
 use Phoenix\Di\Container;
 use Phoenix\Loader\Interfaces\HasLoadCondition;
 use Phoenix\Loader\Interfaces\Loadable;
+use Phoenix\Loader\Traits\CanLoadInitializers;
 
 class Bootstrapper implements Loadable
 {
