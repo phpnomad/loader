12a7a41f43f37bbe2f6ebc59ec49c55e98d81915
Alex Standiford <alex@standiford.us>
2024-01-18 09:48:52 -0500

    Progress

--- lib/Interfaces/HasClassDefinitions.php
+++ lib/Interfaces/HasClassDefinitions.php
@@ -3,5 +3,10 @@
 namespace PHPNomad\Loader\Interfaces;
 interface HasClassDefinitions
 {
+    /**
+     * Specifies class definitions
+     *
+     * @return array<class-string,class-string|class-string[]> One or multiple interfaces that are implemented by the key.
+     */
     public function getClassDefinitions(): array;
 }
